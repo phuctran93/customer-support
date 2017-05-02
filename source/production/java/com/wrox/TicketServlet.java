0ce108c91020229c0500a5320f91d40291da4de3
phuctran93 <phuctran93@hotmail.com>
2017-05-02 16:57:57 +0700

    Configuring the Servlet for File Uploads

--- source/production/java/com/wrox/TicketServlet.java
+++ source/production/java/com/wrox/TicketServlet.java
@@ -1,22 +1,72 @@
 package com.wrox;
 
-import java.io.IOException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+@WebServlet(
+        name = "ticketServlet",
+        urlPatterns = {"/tickets"},
+        loadOnStartup = 1
+)
+@MultipartConfig(
+        fileSizeThreshold = 5_242_880, //5MB
+        maxFileSize = 20_971_520L, //20MB
+        maxRequestSize = 41_943_040L //40MB
+)
 public class TicketServlet extends HttpServlet
 {
+    private volatile int TICKET_ID_SEQUENCE = 1;
+
+    private Map<Integer, Ticket> ticketDatabase = new LinkedHashMap<>();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException
     {
+        String action = request.getParameter("action");
+        if(action == null)
+            action = "list";
+        switch(action)
+        {
+            case "create":
+                this.showTicketForm(response);
+                break;
+            case "view":
+                this.viewTicket(request, response);
+                break;
+            case "download":
+                this.downloadAttachment(request, response);
+                break;
+            case "list":
+            default:
+                this.listTickets(response);
+                break;
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException
     {
+        String action = request.getParameter("action");
+        if(action == null)
+            action = "list";
+        switch(action)
+        {
+            case "create":
+                this.createTicket(request, response);
+                break;
+            case "list":
+            default:
+                response.sendRedirect("tickets");
+                break;
+        }
     }
 }
