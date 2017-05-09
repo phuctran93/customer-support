88058922de3a20dc5d5ee7d3bc714d52076eb43e
phuctran93 <phuctran93@hotmail.com>
2017-05-09 12:13:20 +0700

    use spring form in login page

--- source/production/java/com/wrox/site/AuthenticationFilter.java
+++ source/production/java/com/wrox/site/AuthenticationFilter.java
@@ -19,7 +19,11 @@ public class AuthenticationFilter implements Filter
     {
         HttpSession session = ((HttpServletRequest)request).getSession(false);
         if(session == null || session.getAttribute("username") == null)
-            ((HttpServletResponse)response).sendRedirect("login");
+        {
+            ((HttpServletResponse)response).sendRedirect(
+                    ((HttpServletRequest) request).getContextPath() + "/login"
+            );
+        }
         else
             chain.doFilter(request, response);
     }
