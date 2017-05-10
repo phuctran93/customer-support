e779785349802d955e88a287e4d96be2884358ce
phuctran93 <phuctran93@hotmail.com>
2017-05-10 14:26:37 +0700

    Configure Spring Validator Bean

--- source/production/java/com/wrox/config/RootContextConfiguration.java
+++ source/production/java/com/wrox/config/RootContextConfiguration.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -19,6 +20,8 @@ import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.Executor;
@@ -46,9 +49,26 @@ public class RootContextConfiguration
         messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
         messageSource.setBasenames(
                 "/WEB-INF/i18n/titles", "/WEB-INF/i18n/messages",
-                "/WEB-INF/i18n/errors"
+                "/WEB-INF/i18n/errors", "/WEB-INF/i18n/validation"
         );
         return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean localValidatorFactoryBean()
+    {
+        LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+        validator.setValidationMessageSource(this.messageSource());
+        return validator;
+    }
+
+    @Bean
+    public MethodValidationPostProcessor methodValidationPostProcessor()
+    {
+        MethodValidationPostProcessor processor =
+                new MethodValidationPostProcessor();
+        processor.setValidator(this.localValidatorFactoryBean());
+        return processor;
     }
 
     @Bean
@@ -96,6 +116,12 @@ public class RootContextConfiguration
         Executor executor = this.taskScheduler();
         log.info("Configuring asynchronous method executor {}.", executor);
         return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler()
+    {
+        return null;
     }
 
     @Override
